c358afbebe3d9ea7a2fc8a0b78925b5a17121554
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-09-29 01:49:05 +0300

    priority queue converted into generic form.

--- internal/priority-queue/priority-queue.go
+++ internal/priority-queue/priority-queue.go
@@ -3,47 +3,48 @@ package priority_queue
 import "log"
 
 /* This queue uses priority approach; item has little priority value is more prior.
-*/
-
+ */
+//
 type QueueItem interface {
 	GetPriority() int
 }
-type PriorityQueue struct {
-	items []QueueItem
+type PriorityQueue[T QueueItem] struct {
+	items []T
 }
-func NewPriorityQueue() *PriorityQueue {
-	return &PriorityQueue{}
+
+func NewPriorityQueue[T QueueItem]() *PriorityQueue[T] {
+	return &PriorityQueue[T]{}
 }
-func (p *PriorityQueue) Enqueue(item QueueItem) {
+func (p *PriorityQueue[T]) Enqueue(item T) {
 	i := 0
 	for i < len(p.items) {
 		if p.items[i].GetPriority() < item.GetPriority() {
 			i++
-		}else {
+		} else {
 			break
 		}
 	}
 	if i == len(p.items) {
 		p.items = append(p.items, item)
-	}else {
-		p.items = append(p.items[:i+1],p.items[i:]...)
+	} else {
+		p.items = append(p.items[:i+1], p.items[i:]...)
 		p.items[i] = item
 	}
 }
-func (p *PriorityQueue) Dequeue() QueueItem {
+func (p *PriorityQueue[T]) Dequeue() QueueItem {
 	item := p.items[0]
 	p.items = p.items[1:]
 	return item
 }
-func (p *PriorityQueue) Print() {
+func (p *PriorityQueue[T]) Print() {
 	for index, item := range p.items {
 		log.Printf("\n Index: %d, Item : %v", index, item)
 	}
 }
-func (p *PriorityQueue) GetItems() []QueueItem {
+func (p *PriorityQueue[T]) GetItems() []T {
 	return p.items
 }
 
-func (p *PriorityQueue) Length() int {
+func (p *PriorityQueue[T]) Length() int {
 	return len(p.items)
 }
