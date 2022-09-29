111fa33fc096244462f78ac8c3682ec7edb6eafc
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-09-30 01:31:56 +0300

    comment refactoring

--- internal/util/convertutil/convertutil.go
+++ internal/util/convertutil/convertutil.go
@@ -4,8 +4,8 @@ import "encoding/json"
 
 /*AnyTo generic function takes a
 data and a generic type T. Then tries to convert the given data
-to type of T. It returns nil with error if it cannot convert. Otherwise,
-it returns the data in converted format.*/
+to type of T. It returns nil with error if it cannot convert.
+Otherwise, it returns the data in converted format.*/
 func AnyTo[T any](data any) (T, error) {
 	var to T
 	bytes, err := json.Marshal(data)
